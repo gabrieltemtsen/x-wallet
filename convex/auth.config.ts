f75f6aba9c776ebd2da65e0472bdf116f0b4d660
Gabriel Temtsen <gabrieltemtsen@gmail.com>
2024-04-30 13:52:44 +0100

    updt

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+        providers: [
+            {
+                domain: "dev-3kvo8gurvbtg3koc.us.auth0.com",
+                applicationID: "PHGzChWplNnxn9QWG28nnkNiYVe2hKn2",
+            },
+        ]
+};
+
+export default authConfig;
